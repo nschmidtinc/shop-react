be35676881a1dd76837c5752ec4003aa499190d4
Erin Carrieres <rnncarrieres@gmail.com>
2018-08-12 18:42:17 -0400

    Added more grid

--- src/App.js
+++ src/App.js
@@ -6,14 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
         <div className="container">
           <div class="one-third column">
             <p>Hello</p>
@@ -27,7 +19,7 @@ class App extends Component {
             <p>Yo</p> 
           </div> 
 
-          <div className="one-fifth column">
+          <div className="sixteen columns">
             <p>Yooo</p>
           </div>
         </div> 
